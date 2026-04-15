c8b3825cff7540477af37c797996f6f3c05f5365
Tanju Özer <tanju.ozer64@gmail.com>
2026-04-15 23:47:07 +0200

    feat(controllers): implement createMenu with Cloudinary image integration

--- src/controllers/menu.controller.ts
+++ src/controllers/menu.controller.ts
@@ -2,6 +2,7 @@ import type { Request, Response } from "express";
 import Menu from "../models/Menu.js";
 import { StatusCodes } from "http-status-codes";
 import NotFoundError from "../errors/not-found.js";
+import BadRequest from "../errors/badRequest.js";
 
 export const getAllMenu = async (req: Request, res: Response) => {
   const menu = await Menu.find({});
@@ -18,7 +19,16 @@ export const getSingleMenu = async (req: Request, res: Response) => {
   res.status(StatusCodes.OK).json({ data: menu });
 };
 
-export const createMenu = async (req: Request, res: Response) => {};
+export const createMenu = async (req: Request, res: Response) => {
+  const { title } = req.body;
+  const imageUrl = req.image?.secure_url;
+
+  if (!imageUrl) {
+    throw new BadRequest("Please provide an image");
+  }
+  const menu = await Menu.create({ title, image: imageUrl });
+  res.status(StatusCodes.CREATED).json({ data: menu });
+};
 
 export const updateMenu = async (req: Request, res: Response) => {};
 
