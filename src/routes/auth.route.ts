5a434b7c34c0658a4106a5a7c5d69fa85ea31313
Tanju Özer <tanju.ozer64@gmail.com>
2026-04-15 21:19:06 +0200

    feat(auth): add request validation using Zod schemas

--- src/routes/auth.route.ts
+++ src/routes/auth.route.ts
@@ -5,11 +5,13 @@ import {
   register,
 } from "../controllers/auth.controller.js";
 import { checkAuth } from "../middleware/auth.js";
+import validate from "../middleware/validateResource.js";
+import { loginSchema, registerSchema } from "../schemas/auth.schema.js";
 
 const router = express.Router();
 
-router.post("/login", login);
-router.post("/register", register);
+router.post("/login", validate(loginSchema), login);
+router.post("/register", validate(registerSchema), register);
 router.get("/current", checkAuth, getCurrentUser);
 
 export default router;
