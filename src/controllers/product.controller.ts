66f7ea82957cd19bf578fdb09630927f1113e259
Tanju Özer <tanju.ozer64@gmail.com>
2026-04-16 00:11:50 +0200

    feat(controllers): implement getAllMenuProduct with menu population and lean queries

--- src/controllers/product.controller.ts
+++ src/controllers/product.controller.ts
@@ -2,7 +2,15 @@ import type { Request, Response } from "express";
 import { StatusCodes } from "http-status-codes";
 import Product from "../models/Product.js";
 
-export const getAllMenuProduct = async (req: Request, res: Response) => {};
+export const getAllMenuProduct = async (req: Request, res: Response) => {
+  const { menuId } = req.params as { menuId: string };
+  const products = await Product.find({ menu: menuId }).populate("menu").lean();
+  res.status(StatusCodes.OK).json({
+    success: true,
+    count: products.length,
+    data: products,
+  });
+};
 
 export const getSingleProduct = async (req: Request, res: Response) => {};
 
