f3460d22da1cd1bc67a4d8596b9d847ef161166f
Tanju Özer <tanju.ozer64@gmail.com>
2026-04-16 00:14:03 +0200

    feat(controllers): implement getSingleProduct with menu population

--- src/controllers/product.controller.ts
+++ src/controllers/product.controller.ts
@@ -1,5 +1,6 @@
 import type { Request, Response } from "express";
 import { StatusCodes } from "http-status-codes";
+import NotFoundError from "../errors/not-found.js";
 import Product from "../models/Product.js";
 
 export const getAllMenuProduct = async (req: Request, res: Response) => {
@@ -12,7 +13,18 @@ export const getAllMenuProduct = async (req: Request, res: Response) => {
   });
 };
 
-export const getSingleProduct = async (req: Request, res: Response) => {};
+export const getSingleProduct = async (req: Request, res: Response) => {
+  const { id } = req.params as { id: string };
+  const product = await Product.findById(id).populate("menu").lean();
+
+  if (!product) {
+    throw new NotFoundError(` No product found with id: ${id}`);
+  }
+  res.status(StatusCodes.OK).json({
+    success: true,
+    data: product,
+  });
+};
 
 export const createProduct = async (req: Request, res: Response) => {};
 
